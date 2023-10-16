7a23be8a71d00f7a43ddacdba8376673ac2cc31f
xuc4 <xuc4@mail.uc.edu>
2023-10-16 11:19:43 +0800

    added logger and exception handling messages

--- src/main/java/com/shoppinglist/shoppinglist/service/ShoppingListService.java
+++ src/main/java/com/shoppinglist/shoppinglist/service/ShoppingListService.java
@@ -2,15 +2,19 @@ package com.shoppinglist.shoppinglist.service;
 
 import com.shoppinglist.shoppinglist.dao.IShoppingListDAO;
 import com.shoppinglist.shoppinglist.dto.ListItem;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Component
 public class ShoppingListService implements IShoppingListService {
     // Todo: Add cache annotations
     private final IShoppingListDAO shoppingListDAO;
+    private static final Logger logger = LoggerFactory.getLogger(ShoppingListService.class);
 
     public ShoppingListService(IShoppingListDAO shoppingListDAO) {
         this.shoppingListDAO = shoppingListDAO;
@@ -28,11 +32,22 @@ public class ShoppingListService implements IShoppingListService {
 
     @Override
     public ListItem fetchById(int id) throws NoSuchElementException {
-        return shoppingListDAO.fetch(id);
+        try {
+            return shoppingListDAO.fetch(id);
+        } catch (NoSuchElementException e) {
+            logger.error("Item not found with ID: {}", id);
+            throw e;
+        }
     }
 
     @Override
     public void delete(int id) throws NoSuchElementException {
-        shoppingListDAO.delete(id);
+        try {
+            shoppingListDAO.delete(id);
+            logger.info("Deleted item with ID: {}", id);
+        } catch (NoSuchElementException e) {
+            logger.error("Item not found with ID: {}", id);
+            throw e;
+        }
     }
 }
